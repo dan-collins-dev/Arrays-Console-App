8be176f6ab95fd0962881e6c8c4efa954b78ea39
Dan Collins <dan.collins.dev@gmail.com>
2025-10-29 20:35:18 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ArraysConsoleApp;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Entity[] entities = new Entity[5];
+        string[] names = new string[5];
+        Random numberGenerator = new Random();
+
+        names = ["Goblin", "Bat", "Slime", "Dragon", "Spoony Bard"];
+
+        for (int i = 0; i < entities.Length; i++)
+        {
+            Entity newEntity = new Entity(names[i], numberGenerator.Next(10) + 1);
+            entities[i] = newEntity;
+        }
+
+        Console.WriteLine("============================");
+        Console.WriteLine("=       Entity Stats       =");
+        Console.WriteLine("============================");
+
+        foreach (Entity e in entities)
+            e.DisplayStats();
+    }
+
+    class Entity
+    {
+        private string name;
+        private int healthPoints;
+
+        public Entity(string name = "Unknown Entity", int healthPoints = 9999)
+        {
+            this.name = name;
+            this.healthPoints = healthPoints;
+        }
+
+        // apparently C# has arrow functions. cool.
+        public void SetName(string name) => this.name = name;
+
+        public string GetName() => this.name;
+
+        public void SetHealthPoints(int healthPoints) => this.healthPoints = healthPoints;
+
+        public int GetHealthPoints() => this.healthPoints;
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"Name:\t\t{this.name}");
+            Console.WriteLine($"Health Points:\t{this.healthPoints}");
+            Console.WriteLine("============================");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# Arrays Console App
+
+## Overview
+
+This assignment is a deliverable for [Code:You](https://code-you.org/)'s Software Development with C# pathway. In this assignment we were to create simple program that should:
+
+- Initializes and populates an array with a series of values. It can be an array of anything - just make sure it has more than 2 or 3 values.
+- Uses a `foreach` loop to iterate over those values and display them to the console.
+
+**Note**: I used this as an opportunity to experiment and get my hands dirty. Included an `Entity` class to be the data type that the loop stores and iterates over. Still need to read up on what constructors an object should have and class properties
+
+## To Run Locally
+
+#### Required Tools to Run
+
+| Tools                       |
+| --------------------------- |
+| .NET SDK                    |
+| VS Code                     |
+| C# Dev Kit VSCode Extension |
+
+#### Instructions
+
+1. Verify that you have the .NET SDK installed. To do this, run `dotnet --version` in GitBash. If a version number is not returned, you can [download the .NET SDK here](https://dotnet.microsoft.com/en-us/download).
+2. Once you have verified that the .NET SDK is installed, open GitBash and `cd` into the directory of your choice.
+3. Clone this repository by running `git clone https://github.com/dan-collins-dev/Arrays-Console-App.git` in GitBash
+4. `cd` into the cloned project and open it with VS Code
+5. In VS Code's integrated terminal, run `dotnet build` followed by `dotnet run` to build and run the application.
